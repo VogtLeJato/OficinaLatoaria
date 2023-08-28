40c4cf7232f942c3f08b38ccc0bd92ea4682e035
VogtLeJato <guivogt1@gmail.com>
2023-08-27 22:48:21 -0300

    Criação da migration das novas tabelas.

--- API/API_Oficina/API_Oficina/Migrations/20230828014542_CriacaoRestoDb.Designer.cs
+++ API/API_Oficina/API_Oficina/Migrations/20230828014542_CriacaoRestoDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API_Oficina.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API_Oficina.Migrations
 {
     [DbContext(typeof(OficinaContext))]
-    partial class CarContextModelSnapshot : ModelSnapshot
+    [Migration("20230828014542_CriacaoRestoDb")]
+    partial class CriacaoRestoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/API_Oficina/API_Oficina/Migrations/20230828014542_CriacaoRestoDb.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Oficina.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoRestoDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CPF = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "WorkTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkTypes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Works",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TypeId = table.Column<int>(type: "int", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    Updated = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    IsConcluded = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    LaborPrice = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Works", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Works_WorkTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "WorkTypes",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BuyedMaterials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MaterialId = table.Column<int>(type: "int", nullable: true),
+                    PricePayed = table.Column<long>(type: "bigint", nullable: false),
+                    DateBuyed = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    WorkId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuyedMaterials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BuyedMaterials_Materials_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Materials",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BuyedMaterials_Works_WorkId",
+                        column: x => x.WorkId,
+                        principalTable: "Works",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuyedMaterials_MaterialId",
+                table: "BuyedMaterials",
+                column: "MaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuyedMaterials_WorkId",
+                table: "BuyedMaterials",
+                column: "WorkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Works_TypeId",
+                table: "Works",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BuyedMaterials");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Works");
+
+            migrationBuilder.DropTable(
+                name: "WorkTypes");
+        }
+    }
+}
